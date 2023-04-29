d5ab9f2cf433d71d6aa6b4b56f2da1c4cfb1f867
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-04-29 10:03:41 -0300

    Contexto do EF

---
+++ Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IWantApp.Domain.Products;
+
+namespace IWantApp.Infra.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>()
+            .Property(p => p.Name).IsRequired();
+        builder.Entity<Product>()
+            .Property(p => p.Description).HasMaxLength(255).IsRequired(false);
+        builder.Entity<Category>()
+            .Property(c => c.Name).IsRequired();
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configuration)
+    {
+        configuration.Properties<string>()
+            .HaveMaxLength(100);
+    }
+}
