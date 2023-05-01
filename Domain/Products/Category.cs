cea9c84e684556fa342dfb8a5c8dc6c2200444ff
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-05-01 17:03:22 -0300

    Refatorando o construtor para mais validações

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -4,12 +4,19 @@ namespace IWantApp.Domain.Products;
 
 public class Category : Entity
 {
-    public Category(string name)
+    public Category(string name, string createdBy, string editedBy)
     {
-        var contract = new Contract<Category>().IsNotNullOrEmpty(name, "Name", "Nome é obrigatório");
+        var contract = new Contract<Category>()
+            .IsNotNullOrEmpty(name, "Name", "Nome é obrigatório")
+            .IsNotNullOrEmpty(createdBy, "CreatedBy")
+            .IsNotNullOrEmpty(editedBy, "EditedBy");
         AddNotifications(contract);
         Name = name;
         Active = true;
+        CreatedBy = createdBy;
+        EditedBy = editedBy;
+        CreatedOn = DateTime.Now;
+        EditedOn = DateTime.Now;
     }
     public string Name { get; set; }
     public bool Active { get; set; }
