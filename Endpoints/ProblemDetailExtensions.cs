94a1d418177e88ce8b33078b984ee18670a5ea18
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-05-20 09:19:39 -0300

    Retornando erros com problem details

--- Endpoints/ProblemDetailExtensions.cs
+++ Endpoints/ProblemDetailExtensions.cs
@@ -1,9 +1,18 @@
 ﻿using Flunt.Notifications;
+using Microsoft.AspNetCore.Identity;
 
 namespace IWantApp.Endpoints;
 
 public static class ProblemDetailExtensions
 {
     public static Dictionary<string, string[]> ConvertToProblemDetails(this IReadOnlyCollection<Notification> notifications)
-        => notifications.GroupBy(g => g.Key).ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());    
+        => notifications.GroupBy(g => g.Key).ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
+
+    public static Dictionary<string, string[]> ConvertToProblemDetails(this IEnumerable<IdentityError> error)
+    {
+        var dictionary = new Dictionary<string, string[]>();
+        dictionary.Add("Error", error.Select(e => e.Description).ToArray());
+        return dictionary;
+    }
+        //=> error.GroupBy(g => g.Code).ToDictionary(g => g.Key, g => g.Select(x => x.Description).ToArray());
 }
