02dedfc7cf0712aaafd253c60d4c98b67fb74f8e
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-06-24 08:50:57 -0300

    104. Implicit Using.

--- Endpoints/Security/TokenPost.cs
+++ Endpoints/Security/TokenPost.cs
@@ -1,12 +1,4 @@
-﻿using IWantApp.Endpoints.Security;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-
-namespace IWantApp.Endpoints.Scurity;
+﻿namespace IWantApp.Endpoints.Scurity;
 
 public class TokenPost
 {
