c69e368e173b5aadd30cc93b057fe4109f3ecbb0
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-07-22 11:35:29 -0300

    117. EF AsNoTracking

--- Endpoints/Products/ProductGetShowcase.cs
+++ Endpoints/Products/ProductGetShowcase.cs
@@ -11,7 +11,7 @@ public class ProductGetShowcase
     {
         if (row > 10) return Results.Problem(title: "Row with max 10", statusCode: 400);
 
-        var queryBase = context.Products.Include(p => p.Category)
+        var queryBase = context.Products.AsNoTracking().Include(p => p.Category)
             .Where(p => p.HasStock && p.Category.Active);
 
         queryBase = orderBy == "name" ? queryBase.OrderBy(p => p.Name) : queryBase.OrderBy(p => p.Price);
@@ -31,7 +31,7 @@ public class ProductGetShowcase
 
         var queryFilter = queryBase.Skip((page - 1) * row).Take(row);
 
-        var products = await queryFilter.ToArrayAsync();
+        var products = await queryFilter.ToListAsync();
 
         var results = products.Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Active));
         return Results.Ok(results);
