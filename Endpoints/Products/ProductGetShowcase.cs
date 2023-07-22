dbe90110c1f15f3d047864999fd116d8e187e19e
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-07-22 11:17:46 -0300

    115. Paginação

--- Endpoints/Products/ProductGetShowcase.cs
+++ Endpoints/Products/ProductGetShowcase.cs
@@ -7,12 +7,22 @@ public class ProductGetShowcase
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static async Task<IResult> Action(ApplicationDbContext context)
+    public static async Task<IResult> Action(int? page, int? row, string? orderBy, ApplicationDbContext context)
     {
-        var products = await context.Products.Include(p => p.Category)
-            .Where(p => p.HasStock && p.Category.Active)
-            .OrderBy(p => p.Name).ToListAsync();
-        var results = products.Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Active ));
+        if (page == null) page = 1;
+        if (row == null) row = 10;
+        if (string.IsNullOrEmpty(orderBy)) orderBy = "name";
+
+        var queryBase = context.Products.Include(p => p.Category)
+            .Where(p => p.HasStock && p.Category.Active);
+
+        queryBase = orderBy == "name" ? queryBase.OrderBy(p => p.Name) : queryBase.OrderBy(p => p.Price);
+
+        var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+
+        var products = await queryFilter.ToArrayAsync();
+
+        var results = products.Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Active));
         return Results.Ok(results);
     }
 }
