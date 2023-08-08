3ee1424f7f02d67ff4c8a84252186cf83894df5e
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-08-07 21:12:36 -0300

    126. Validação final.

--- Endpoints/Orders/OrderPost.cs
+++ Endpoints/Orders/OrderPost.cs
@@ -8,16 +8,25 @@ public class OrderPost
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
 
-    [Authorize]
+    [Authorize(Policy = "CpfPolicy")]
     public static async Task<IResult> Action(OrderRequest orderRequest, HttpContext http, ApplicationDbContext context)
     {   
         var clientId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
         var clientName = http.User.Claims.First(c => c.Type == "Name").Value;
 
-        var products = new List<Product>();
-        var productsFound = context.Products.Where(p => orderRequest.ProductsIds.Contains(p.Id)).ToList();
+        //if (orderRequest.ProductsIds == null || !orderRequest.ProductsIds.Any())
+        //    return Results.BadRequest("Produto é obrigatório para pedido.");
+
+        //if(string.IsNullOrEmpty(orderRequest.DelivceryAddress))
+        //    return Results.BadRequest("Endereço de entrega é obrigatório.");
+
+        List<Product> productsFound = null;
+        if (orderRequest.ProductsIds != null && orderRequest.ProductsIds.Any())
+             productsFound = context.Products.Where(p => orderRequest.ProductsIds.Contains(p.Id)).ToList();               
         
         var order = new Order(clientId, clientName, productsFound, orderRequest.DelivceryAddress);
+        if (!order.IsValid)
+            return Results.ValidationProblem(order.Notifications.ConvertToProblemDetails());
 
         await context.Orders.AddAsync(order);
         await context.SaveChangesAsync();
